0b6f6702c92db2b206059d5bf3cef6227ce7e194
HytOWN <hytmiao@outlook.com>
2019-10-25 21:59:58 +0800

    新建Tools.c来储存工具性质的函数.

---
+++ Homework/20191025/Tools.c
@@ -0,0 +1,30 @@
+#include <windows.h>
+
+/*设置光标位置*/
+void SetPos(int x,int y)
+{
+    COORD pos;
+HANDLE handle;
+    pos.X=x;
+    pos.Y=y;
+    handle=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(handle,pos);
+}
+
+/*设置颜色*/
+void SetColor(unsigned short ForeColor,unsigned short BackGroundColor)
+{
+/*
+对应的颜色码表：
+0 = 黑色       8 = 灰色
+1 = 蓝色       9 = 淡蓝色
+2 = 绿色       10 = 淡绿色
+3 = 浅绿色     11 = 淡浅绿色
+4 = 红色       12 = 淡红色
+5 = 紫色       13 = 淡紫色
+6 = 黄色       14 = 淡黄色
+7 = 白色       15 = 亮白色
+*/
+HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);/*获取当前窗口句柄*/
+SetConsoleTextAttribute(handle,ForeColor+(BackGroundColor<<4));/*设置颜色*/
+}
--- Homework/20191025/main.c
+++ Homework/20191025/main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <conio.h>
-#include <windows.h>
 
 /*打印数组所有元素*/
 void PrintArray(int a[],int len)
@@ -40,23 +39,6 @@ void PrtAryHiLi(int a[],int len,int hiLi1,int hiLi2)
             //textbackground();
         }
     }
-}
-
-void setColor(unsigned short ForeColor,unsigned short BackGroundColor)
-{
-/*
-对应的颜色码表：
-0 = 黑色       8 = 灰色
-1 = 蓝色       9 = 淡蓝色
-2 = 绿色       10 = 淡绿色
-3 = 浅绿色     11 = 淡浅绿色
-4 = 红色       12 = 淡红色
-5 = 紫色       13 = 淡紫色
-6 = 黄色       14 = 淡黄色
-7 = 白色       15 = 亮白色
-*/
-HANDLE handle=GetStdHandle(STD_OUTPUT_HANDLE);/*获取当前窗口句柄*/
-SetConsoleTextAttribute(handle,ForeColor+BackGroundColor<<4);/*设置颜色*/
 }
 
 int main()
