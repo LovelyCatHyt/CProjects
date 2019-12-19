e733dd08ad7cbe39d7bb320d1ee616e6f98cee4e
LovelyCatHyt <hyt@paracraft.club>
2019-12-19 11:07:10 +0800

    现在可以用字母定位菜单了.

--- PayrollSystem/Toolkit/MenuMgr.c
+++ PayrollSystem/Toolkit/MenuMgr.c
@@ -28,24 +28,27 @@ void (*ShowMenu(COORD cdBeginPos, MenuNode *contents, size_t arraySize))(void)
     COORD cdCurrentPos;
     //目标坐标
     COORD cdTargetPos;
-    SHORT maxWidth = 0;
+    SHORT menuWidth = 0;
     //启用循环
     BOOL canLoop = TRUE;
     void (*action)(void) = NULL;
     SetPos(cdBeginPos);
     for(i = 0,cdCurrentPos = cdBeginPos;i<arraySize;i++,cdCurrentPos.Y++)
     {
+        //遍历输出每个菜单项
         SetPos(cdCurrentPos);
-        printf("%s",contents[i].lable);
-        SHORT tmpLen = strlen(contents[i].lable);
-        maxWidth = tmpLen>maxWidth?tmpLen:maxWidth;
+        //用字母编号 暂时不管菜单数量大于26的情况
+        printf("[%c]%s",i+'a',contents[i].lable);
+        SHORT tmpLen = strlen(contents[i].lable) + 3;
+        menuWidth = tmpLen>menuWidth ? tmpLen : menuWidth;
     }
     //高亮
     cdCurrentPos = cdBeginPos;
-    SetRectAttr(cdMenuCursor,SHORT2COORD(cdMenuCursor.X + maxWidth - 1,cdMenuCursor.Y),highlightAttr);
+    SetRectAttr(cdMenuCursor,SHORT2COORD(cdMenuCursor.X + menuWidth - 1,cdMenuCursor.Y),highlightAttr);
     while(canLoop)
     {
-        switch(GetKeyBoardInput())
+        int input = GetKeyBoardInput();
+        switch(input)
         {
         case keycode_UpArrow:
             //向上
@@ -60,7 +63,7 @@ void (*ShowMenu(COORD cdBeginPos, MenuNode *contents, size_t arraySize))(void)
             }
             cdTargetPos.Y = cdBeginPos.Y + sSelectingMenuID;
             //移动高亮
-            SwitchMenuHighlight(cdCurrentPos,cdTargetPos,maxWidth);
+            SwitchMenuHighlight(cdCurrentPos,cdTargetPos,menuWidth);
             cdCurrentPos = cdTargetPos;
             break;
         case keycode_DownArrow:
@@ -76,7 +79,7 @@ void (*ShowMenu(COORD cdBeginPos, MenuNode *contents, size_t arraySize))(void)
             }
             cdTargetPos.Y = cdBeginPos.Y + sSelectingMenuID;
             //移动高亮
-            SwitchMenuHighlight(cdCurrentPos,cdTargetPos,maxWidth);
+            SwitchMenuHighlight(cdCurrentPos,cdTargetPos,menuWidth);
             cdCurrentPos = cdTargetPos;
             break;
         case keycode_RightArrow:
@@ -89,16 +92,16 @@ void (*ShowMenu(COORD cdBeginPos, MenuNode *contents, size_t arraySize))(void)
             {
                 //进入子菜单
                 //没有执行任务则继续循环
-                action = ShowMenu(SHORT2COORD(cdCurrentPos.X + maxWidth + 1, cdCurrentPos.Y),
+                action = ShowMenu(SHORT2COORD(cdCurrentPos.X + menuWidth + 1, cdCurrentPos.Y),
                     contents[sSelectingMenuID].childs,
                     contents[sSelectingMenuID].childNum);
                 if(action != NULL)
                 {
                     //返回的函数不为空
                     //清除菜单内容
-                    SetRectChar(cdBeginPos,SHORT2COORD(cdBeginPos.X + maxWidth - 1,cdBeginPos.Y + arraySize - 1),' ');
+                    SetRectChar(cdBeginPos,SHORT2COORD(cdBeginPos.X + menuWidth - 1,cdBeginPos.Y + arraySize - 1),' ');
                     //清除菜单颜色
-                    SetRectAttr(cdBeginPos,SHORT2COORD(cdBeginPos.X + maxWidth - 1,cdBeginPos.Y + arraySize - 1),defaultAttr);
+                    SetRectAttr(cdBeginPos,SHORT2COORD(cdBeginPos.X + menuWidth - 1,cdBeginPos.Y + arraySize - 1),defaultAttr);
                     //返回起始点
                     SetPos(cdBeginPos);
                     canLoop = FALSE;
@@ -115,9 +118,9 @@ void (*ShowMenu(COORD cdBeginPos, MenuNode *contents, size_t arraySize))(void)
             }else
             {
                 //清除菜单内容
-                SetRectChar(cdBeginPos,SHORT2COORD(cdBeginPos.X + maxWidth - 1,cdBeginPos.Y + arraySize - 1),' ');
+                SetRectChar(cdBeginPos,SHORT2COORD(cdBeginPos.X + menuWidth - 1,cdBeginPos.Y + arraySize - 1),' ');
                 //清除菜单颜色
-                SetRectAttr(cdBeginPos,SHORT2COORD(cdBeginPos.X + maxWidth - 1,cdBeginPos.Y + arraySize - 1),defaultAttr);
+                SetRectAttr(cdBeginPos,SHORT2COORD(cdBeginPos.X + menuWidth - 1,cdBeginPos.Y + arraySize - 1),defaultAttr);
                 //返回起始点
                 SetPos(cdBeginPos);
                 canLoop = FALSE;
@@ -133,13 +136,30 @@ void (*ShowMenu(COORD cdBeginPos, MenuNode *contents, size_t arraySize))(void)
             {
                 //不是根菜单
                 //清除菜单内容
-                SetRectChar(cdBeginPos,SHORT2COORD(cdBeginPos.X + maxWidth - 1,cdBeginPos.Y + arraySize - 1),' ');
+                SetRectChar(cdBeginPos,SHORT2COORD(cdBeginPos.X + menuWidth - 1,cdBeginPos.Y + arraySize - 1),' ');
                 //清除菜单颜色
-                SetRectAttr(cdBeginPos,SHORT2COORD(cdBeginPos.X + maxWidth - 1,cdBeginPos.Y + arraySize - 1),defaultAttr);
+                SetRectAttr(cdBeginPos,SHORT2COORD(cdBeginPos.X + menuWidth - 1,cdBeginPos.Y + arraySize - 1),defaultAttr);
                 //返回到初始坐标
                 SetPos(cdBeginPos);
                 //禁用循环以结束当前菜单
                 canLoop = FALSE;
+            }
+            break;
+        default:
+            //其他情况,支持用字母定位菜单
+            if(input>='A' && input <='Z')
+            {
+                //转换成小写
+                input+='a'-'A';
+            }
+            if(input>='a' && input<='z' && (input - 'a')<arraySize)
+            {
+                //跳转到对应菜单并更新相关参数
+                cdTargetPos = cdBeginPos;
+                sSelectingMenuID = input - 'a';
+                cdTargetPos.Y += sSelectingMenuID;
+                SwitchMenuHighlight(cdCurrentPos,cdTargetPos,menuWidth);
+                cdCurrentPos = cdTargetPos;
             }
             break;
         }
